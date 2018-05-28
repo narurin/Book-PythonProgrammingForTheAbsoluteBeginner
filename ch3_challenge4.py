644bf4fb9d4ddf6c83e94a09bdb1df3a60141288
narurin <39412476+narurin@users.noreply.github.com>
2018-05-28 22:43:34 +0800

    revise code

--- ch3_challenge4.py
+++ ch3_challenge4.py
@@ -2,42 +2,44 @@
 #for computer to guess
 
 import random
+
+upper_limit = 100
+lower_limit = 1
+attempt_limit = 5
+
 print("\tWelcome to 'Guess My Number'!")
-print("\nInput a number between 1 and 100.")
-print("Lets see if the computer can guess it in 10 tries.\n")
+print("\nInput a number between",lower_limit,"and",upper_limit,".")
+print("Lets see if the computer can guess it in",attempt_limit,"tries.\n")
 
 
 number = int(input("What's your number? "))
 
-if number > 100 or number < 1:
-    print("Please enter a number between 1 and 100.")
+if number > upper_limit or number < lower_limit:
+    print("Please enter a number between",lower_limit,"and",upper_limit,".")
     number = int(input("What's your number? "))
 
-guess = random.randint(1, 100)
-limit = 10
+guess = (upper_limit + lower_limit) // 2
 tries = 1
 
-
-while tries <= limit:
-    if guess != number:
-        if guess > number:
-            print("Lower!\n")
-            guess = random.randint((guess-20), guess)
-        elif guess < number:
-            print("Higher!\n")
-            guess = random.randint(guess, (guess+20))
-            
-        tries += 1
-
-    else:
+while tries <= attempt_limit:
+    print("guess:", guess)
+    if guess == number:
         print("The computer took",tries,"guesses to get the answer!")
         input("\n\nPress the enter key to exit.")
         break
 
-    print(guess)
+    else:
+        if guess > number:
+            print("Lower!\n")
+            upper_limit = guess - 1
+        elif guess < number:
+            print("Higher!\n")
+            lower_limit = guess + 1
+        guess = (upper_limit + lower_limit) // 2           
+        tries += 1
 
-
-print("\nToo bad! The computer couldn't get the answer within 10 tries!")
-input("\n\nPress the enter key to exit.")
+if tries > attempt_limit:
+    print("\nToo bad! The computer couldn't get the answer within",attempt_limit,"tries!")
+    input("\n\nPress the enter key to exit.")
 
 
